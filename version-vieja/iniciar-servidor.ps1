319448a25e77a144e7a2a449f0e61fd0e18c6aed
cdiguardia <cguardia@gmail.com>
2026-05-22 12:06:35 -0300

    borando versión vieja

--- version-vieja/iniciar-servidor.ps1
+++
@@ -1,8 +0,0 @@
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location $scriptDir
-Start-Process "http://localhost:3000/novedades.html"
-if (Get-Command py -ErrorAction SilentlyContinue) {
-    py -3 .\_server.py
-} else {
-    python .\_server.py
-}
