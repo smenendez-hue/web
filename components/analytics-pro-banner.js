28ec4a3cbf77ecd71511bde90451e3c7d7e74d78
Seba <smenendez@yiqi.com.ar>
2026-05-25 11:06:09 -0300

    link

--- components/analytics-pro-banner.js
+++ components/analytics-pro-banner.js
@@ -443,8 +443,8 @@ html[data-theme="light"] .ds-pro-mock {
         </li>
       </ul>
 
-      <a class="ds-pro-cta" href="${previewUrl}" data-pro-preview="${previewUrl}">
-        Ver demo en vivo
+      <a class="ds-pro-cta" href="https://calendly.com/javierperez/meet-30-demo" target="_blank" rel="noopener">
+        Reservar demo
         <svg viewBox="0 0 24 24" aria-hidden="true"><line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/></svg>
       </a>
     </div>
